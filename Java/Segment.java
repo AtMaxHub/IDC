bc6be2941504fc215d45ebeb29ca4ca2f5a3d4a2
AtMaxHub <1250828675@qq.com>
2018-05-31 16:35:55 +0800

    SpringMVC 原理 # payment.bak

---
+++ Java/Segment.java
@@ -0,0 +1,43 @@
+	@RequestMapping(value = "/exitNow", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public String exitNow(String orderid) {
+		return gameCtrlService.exitNow(orderid);
+	}
+	
+	private void updateRedisPay(TbOrder order) {
+		Map<String, String> redisMap = jedisClientDao.hgetAll(order.getSongid());
+		String isPayVal = order.getId();
+		String timeLongVal = order.getTimelong();
+		String oldOrderid = redisMap.get(RedisCnst.REDIS_isPay_KEY);
+		long redisRemainTimeLong = 0;
+		if (StringUtils.isNotBlank(oldOrderid)) {
+			redisRemainTimeLong = jedisClientDao.ttl(oldOrderid);
+			if (redisRemainTimeLong > 0) {
+				isPayVal += ("," + oldOrderid);
+				timeLongVal += (redisRemainTimeLong / 60);
+			}
+		}
+		redisMap.put(RedisCnst.REDIS_isPay_KEY, isPayVal);
+		redisMap.put(RedisCnst.REDIS_timeLong_KEY, timeLongVal);
+		/*String flag = jedisClientDao.hmset(order.getSongid(), redisMap);
+		int expireExpressedInSeconds = (Integer.parseInt(order.getTimelong()) - 1) * 60; */
+		int expireExpressedInSeconds = ((Integer.parseInt(order.getTimelong())) * 60) + (int) redisRemainTimeLong;
+		jedisClientDao.setExByExpireValueCompareToZero(expireExpressedInSeconds, redisMap, order);
+	}
+	
+	
+	updateWcPayCallbackHandler
+	
+		public void setExByExpireValueCompareToZero(int expireExpressedInSeconds, Map<String, String> redisMap, TbOrder order) {
+		LOG.debug(order.getId() + ":" + expireExpressedInSeconds);
+		if (expireExpressedInSeconds > 0) {
+			setex(order.getId(), expireExpressedInSeconds, order.getSongid());
+		} else {// 订单已过期
+			redisMap.put(RedisCnst.REDIS_isPay_KEY, "");
+			redisMap.put(RedisCnst.REDIS_timeLong_KEY, "");
+			String res = hmset(order.getSongid(), redisMap);
+			if (LOG.isInfoEnabled()) {
+				LOG.info(" hmset = {} res = {}", JSON.toJSONString(redisMap), res);
+			}
+		}
+	}
