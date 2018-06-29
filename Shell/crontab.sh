3cdb0c9d557f08581ae25c8b579594953f466f1f
AtMaxHub <1250828675@qq.com>
2018-06-29 10:39:08 +0800

    crontab cutNginxLog

---
+++ Shell/crontab.sh
@@ -0,0 +1,4 @@
+MAILTO=15116381886@163.com
+#15,16 15 * * * echo "Good morning." >> /tmp/test.txt
+#50 10 * * * /usr/bin/ls -lh /usr/local/nginx/logs/
+30 1 * * * /usr/bin/ls -lh /usr/local/nginx/logs/;/usr/local/nginx/cronNginx.sh
